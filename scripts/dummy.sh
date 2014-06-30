2cb53e6d26aa20c93aad1216adbdb22ded6f4649
taivop <tpungas@ut.ee>
2014-06-30 21:29:56 +0300

    dummy update

--- scripts/dummy.sh
+++ scripts/dummy.sh
@@ -1,3 +1,7 @@
 cd ..
-sbatch -N 1 --mem=4096 -t 00:00:30 -p testing
-/storage/software/MATLAB_R2013b/bin/matlab -nodisplay -nosplash -nojvm -r "SingleNeuron_IF_Taivo(2000, 10, 1, 'cluster_dummy')"
+#SBATCH -N 1
+#SBATCH --mem=4096
+#SBATCH -t 00:00:30
+#SBATCH -p testing
+
+srun /storage/software/MATLAB_R2013b/bin/matlab -nodisplay -nosplash -nojvm -r "SingleNeuron_IF_Taivo(2000, 10, 1, 'cluster_dummy')"
