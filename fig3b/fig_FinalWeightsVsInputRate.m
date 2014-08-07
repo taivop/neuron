3c99dd9f0a24bc7f1cc1d427698b5c86ffb52540
taivop <tpungas@ut.ee>
2014-08-07 15:51:49 +0300

    fig 3b

---
+++ fig3b/fig_FinalWeightsVsInputRate.m
@@ -0,0 +1,19 @@
+function [] = fig_MeanFinalWeightVsInputRate(inputFreqs1, meanWeights1, inputFreqs2, meanWeights2, inputFreqs3, meanWeights3)
+
+
+figure();
+
+plot(inputFreqs1, meanWeights1, ...
+    'Color','black','Marker','.','MarkerSize',15.0,'LineStyle','-','LineWidth',1.5);
+hold on;
+plot(inputFreqs2, meanWeights2, ...
+    'Color','red','Marker','.','MarkerSize',15.0,'LineStyle','-','LineWidth',1.5);
+plot(inputFreqs3, meanWeights3, ...
+    'Color','blue','Marker','.','MarkerSize',15.0,'LineStyle','-','LineWidth',1.5);
+
+fprintf('input 1 - black, input 2 - red, input 3 - blue\n');
+
+xlabel('Mean input activity (Hz)');
+ylabel('Mean final weight');
+
+title('Mean final weight');
