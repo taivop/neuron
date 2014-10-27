08e758a0a4bdd428f89da86f1883ed377e9e8b40
taivop <tpungas@ut.ee>
2014-10-27 20:21:58 +0200

    correlated input should be generated correctly

--- GenerateInputSpikes.m
+++ GenerateInputSpikes.m
@@ -14,63 +14,38 @@ function [spikes_binary, spiketimes] = GenerateInputSpikes(nrSpikeTrains, rate_t
 
 rate = rate_total;
 
-if (c == 0)
-    % how many spikes are we generating in total?
-    num_all_spikes = round(rate*T0/1000);
-    
-    % assign each spike to some synapse
-    spikes_assigned = randi(nrSpikeTrains,num_all_spikes,1);
-    
-    % initialise array of spiketimes; width is num_all_spikes
-    spiketimes = zeros(nrSpikeTrains, num_all_spikes);
-    
-    spikes_binary = zeros(nrSpikeTrains, T0/dt);
-    for i=1: nrSpikeTrains
-        % how many spikes does this particular synapse get?
-        num_spikes_for_this = size(spikes_assigned(spikes_assigned == i),1);
-        %fprintf('synapse %d, %d spikes\n', i, num_spikes_for_this);
-        
-        % divide the spikes assigned to this synapse randomly over time T0
-        spiketime_vector = sort(rand(1, num_spikes_for_this) * T0/dt);
-        
-        % zero-pad the vector and add to spiketimes matrix
-        spiketimes(i,:) = horzcat(spiketime_vector, zeros(1,num_all_spikes-num_spikes_for_this)); 
-        
-        for j=1:size(spiketimes,2)
-            if spiketimes(i,j) ~= 0
-                spikes_binary(i,ceil(spiketimes(i,j):spiketimes(i,j)+1/dt-1)) = 1;
-            end;
-        end
-    end
-    spikes_binary(:,(T0/dt+1):end) = [];
-    
-    % crop un-necessary zeros from spiketimes matrix
-    [nonZeroRows nonZeroColumns] = find(spiketimes);
-    rightColumn = max(nonZeroColumns(:));
-    spiketimes = spiketimes(1:end, 1:rightColumn);       
-    
-else
-    
-    % rate = rate_total / nrSpikeTrains; % divide the spikes between all synapses
-    N0 = nrSpikeTrains + sqrt(c) * (1 - nrSpikeTrains);
-    
-    Poisson_event_rate = rate;
-    Poisson_event_times = sort(rand(1, round(Poisson_event_rate*T0/1000)) * T0/dt);
-    
-    % assign Poisson events to synapses somehow
-    spikes_binary = zeros(nrSpikeTrains, T0/dt);
-    
-    for i=1: nrSpikeTrains
-        aux = spiketimesMother(rand(size(spiketimesMother))<c);
-        spiketimes(i,1:length(aux)) = aux;
-     
-        for j=1:length(aux)
-            spikes_binary(i,ceil(aux(j):aux(j)+1/dt-1)) = 1;
-        end
-    end
-    spikes_binary(:,(T0/dt+1):end) = [];
-end
+% rate = rate_total / nrSpikeTrains; % divide the spikes between all synapses
+N0 = round(nrSpikeTrains + sqrt(c) * (1 - nrSpikeTrains));
+%fprintf('N0 = %d\n', N0);
 
+% pregenerate Poissonian trains and put the spiketimes one big vector
+spktimes_Poisson = [];
+for i=1:N0
+    SpkTime = HomoPoisSpkGenTaivo(rate, T0/1000, dt);
+    spktimes_Poisson = [spktimes_Poisson; SpkTime];
+end;
+
+% sort spike times
+spktimes_Poisson = sort(spktimes_Poisson);
+
+% round spikes to nearest timebin
+spktimes_Poisson = round(spktimes_Poisson);
+
+% assign each spike to some spike train
+master_mask = randi([1,nrSpikeTrains],size(spktimes_Poisson));
+
+for i=1: nrSpikeTrains
+    mySpikes = spktimes_Poisson(master_mask == i);
+    spiketimes(i,1:length(mySpikes)) = mySpikes;
+
+    for j=1:length(mySpikes)
+        spikes_binary(i,ceil(mySpikes(j):mySpikes(j)+1/dt-1)) = 1;
+    end
+end
+spikes_binary(:,(T0/dt+1):end) = [];
+
+
+% Save to file if necessary
 if (fileName ~= 0)
 	cd data_in;
 	save('-mat7-binary', fileName, 'spikes_binary', 'spiketimes');
