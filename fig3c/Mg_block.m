da46b20eedfd52079fdf5019c6dfbf472f83c14d
taivop <tpungas@ut.ee>
2014-10-27 20:32:19 +0200

    removing directories containing code and figures for reproducing article

--- fig3c/Mg_block.m
+++
@@ -1,4 +0,0 @@
-function B = Mg_block(V)
-Mg_con = 1;
-B = 1./(1+exp(-0.062*V)*(Mg_con/3.57));
-end
--- fig3c/STDP_SingleRun.m
+++
@@ -1,7 +0,0 @@
-function [] = STDP_SingleRun(deltaT, filename_base)
-T_sec = 1;    % run for how many seconds?
-
-fprintf('----- RUNNING experiment with deltaT = %dms -----\n', deltaT);
-filename_spec = sprintf('%s_%dms', filename_base,deltaT);
-
-[g_plas_history, rate_Output] = SingleNeuron_IF_Taivo_STDP(T_sec, deltaT, filename_spec);
