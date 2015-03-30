2c0c9a3f048cf2a86be3e659199305c9f5c8c1c7
taivop <tpungas@ut.ee>
2015-03-30 14:09:23 +0300

    update group input generation to work with 1 synapse

--- input_generation/GenerateInputSpikesGroups.m
+++ input_generation/GenerateInputSpikesGroups.m
@@ -3,25 +3,29 @@
 
 function [spikes_binary, spiketimes] = GenerateInputSpikesGroups(nrSpikeTrains, rate_per_syn, c, T0, dt, fileName)
 
-nrTrainsInGroup1 = round(nrSpikeTrains / 2);
+if nrSpikeTrains == 1
+    [spikes_binary, spiketimes] = GenerateInputSpikesMacke(nrSpikeTrains, rate_per_syn, c, T0, dt, fileName);
+else
+    nrTrainsInGroup1 = round(nrSpikeTrains / 2);
 
-[b1, t1] = GenerateInputSpikesMacke(nrTrainsInGroup1, rate_per_syn * 2, c, T0, dt, '');
-[b2, t2] = GenerateInputSpikesMacke(nrSpikeTrains - nrTrainsInGroup1, rate_per_syn, c, T0, dt, '');
+    [b1, t1] = GenerateInputSpikesMacke(nrTrainsInGroup1, rate_per_syn * 4, c, T0, dt, '');
+    [b2, t2] = GenerateInputSpikesMacke(nrSpikeTrains - nrTrainsInGroup1, rate_per_syn, c, T0, dt, '');
 
-spikes_binary = vertcat(b1, b2);
+    spikes_binary = vertcat(b1, b2);
 
-% pad t1 or t2 with zeros to vertcat them
-if size(t1, 2) > size(t2, 2)
-    % need to pad t2
-    pad_extent = abs(size(t1, 2) - size(t2, 2));
-    t2 = [t2 zeros(size(t2,1), pad_extent)];
-elseif size(t1, 2) < size(t2, 2)
-    % need to pad t1
-    pad_extent = abs(size(t1, 2) - size(t2, 2));
-    t1 = [t1 zeros(size(t1,1), pad_extent)];
+    % pad t1 or t2 with zeros to vertcat them
+    if size(t1, 2) > size(t2, 2)
+        % need to pad t2
+        pad_extent = abs(size(t1, 2) - size(t2, 2));
+        t2 = [t2 zeros(size(t2,1), pad_extent)];
+    elseif size(t1, 2) < size(t2, 2)
+        % need to pad t1
+        pad_extent = abs(size(t1, 2) - size(t2, 2));
+        t1 = [t1 zeros(size(t1,1), pad_extent)];
+    end;
+
+    spiketimes = vertcat(t1, t2);
 end;
-
-spiketimes = vertcat(t1, t2);
 
 end
 
