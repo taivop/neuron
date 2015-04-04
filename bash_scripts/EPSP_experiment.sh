b367ff2783af6e0169f673f1c6a61d5f60e10470
taivop <tpungas@ut.ee>
2015-04-04 17:58:41 +0300

    EPSP experiment bash scripts

---
+++ bash_scripts/EPSP_experiment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ../tests
+
+# Take one node
+#SBATCH -N 1
+
+# Take some CPUs
+#SBATCH -c 2
+
+# Take some memory
+#SBATCH --mem=16000
+
+# Set walltime
+#SBATCH -t 03:00:00
+
+# Set queue
+#SBATCH -p testing
+
+# Set array of amp indexes
+#SBATCH --array=1-7
+
+# amp indexes 1-7
+srun single_EPSP_iocurve
+
+exit
---
+++ bash_scripts/single_EPSP_iocurve.sh
@@ -0,0 +1,1 @@
+srun /storage/software/MATLAB_R2013b/bin/matlab -nodisplay -nosplash -nojvm -r "exp_InputRate_EPSP($SLURM_ARRAY_TASK_ID)"
