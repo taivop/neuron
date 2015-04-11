deccfd45b11263e43c3bbf31b64a49249695de99
taivop <tpungas@ut.ee>
2015-04-11 13:31:25 +0300

    STDP curve figure drawing script

---
+++ figure_scripts/fig_STDPcurve.m
@@ -0,0 +1,11 @@
+function [f] = fig_STDPcurve(deltaTvalues, meanWeights)
+
+plot(deltaTvalues,meanWeights,'k.-','LineWidth',3)
+hold on;
+plot(deltaTvalues,meanWeights,'kx','MarkerSize',10)
+
+xlabel('post-pre deltaT (ms)')
+ylabel('mean final weight')
+title('(Fig. 3c) STDP curve, 100 second simulation, 1Hz regime')
+
+end
