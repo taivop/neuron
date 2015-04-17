ebfca3a2ae0fb8097d7530968d03db2badadd8d5
taivop <tpungas@ut.ee>
2015-04-17 22:16:56 +0300

    smoothing window to parameter

--- figure_scripts/fig_Output.m
+++ figure_scripts/fig_Output.m
@@ -1,4 +1,4 @@
-function [f] = fig_Output(spikes_post, T0)
+function [f] = fig_Output(spikes_post, T0, smooth_window)
     % Plots smoothed mean firing rate.
     % Assumes dt=0.1.
     % At edges, the rate is calculated over only a handful of timesteps, so
@@ -6,14 +6,17 @@ function [f] = fig_Output(spikes_post, T0)
     % zones are not shown.
 
     dt = 0.1;
-    
-    smooth_window = 2; % in seconds
 
     time_in_sec = T0 / 1000;
 
     bool = zeros(1, T0/dt);
     bool(spikes_post) = 1000/dt;
-    smoothed = smooth(bool, smooth_window*1000/dt);
+    
+    if smooth_window == 0
+        smoothed = bool;
+    else
+        smoothed = smooth(bool, smooth_window*1000/dt);
+    end;
 
     % Make x-axis for showing time in seconds
     times = linspace(0, time_in_sec, size(smoothed, 1));
@@ -24,7 +27,7 @@ function [f] = fig_Output(spikes_post, T0)
     plot(times, smoothed);
     xlabel('Time, s');
     ylabel('Output rate, Hz');
-    title(sprintf('Instantaneous output rate, smoothed with a %ds sliding time window', smooth_window));
+    title(sprintf('Instantaneous output rate, smoothed with a %.1fs sliding time window', smooth_window));
     
     xlim([1 time_in_sec-1]);
     %line([1 1], [0 200], 'LineStyle', '--');
