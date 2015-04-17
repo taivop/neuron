a8571cac13b7431e9c02b6adcd133a75abc38863
taivop <tpungas@ut.ee>
2015-04-17 16:02:18 +0300

    exp4: make easier to generate state files

--- exp4/exp4.m
+++ exp4/exp4.m
@@ -7,28 +7,33 @@ T_sec = 5;
 counts_group1 = 10:5:50;
 rates_output = [];
 
+generating_states = 1;
+
 %%
-for j=1:size(counts_group1, 2)
-   fprintf('---Running experiment with %d-synapse group 1.', counts_group1(j));
-   
+for j=1:size(counts_group1, 2)   
    exp.count_group1 = counts_group1(j);
    exp.count_group2 = 100 - exp.count_group1;
    exp.rate_group2 = 10;
    exp.rate_group1 = (1750 - exp.rate_group2 * (exp.count_group2))/exp.count_group1;
    
-   % Creating state files to load from
-   %outFileName = sprintf('exp4_state_width%dsyn', exp.count_group1);
-   %SingleNeuron_IF_Taivo(20, 0, outFileName, 'experiment', exp);
-   
-   % Running 
-   stateFile = sprintf('exp4/state_files/out_exp4_state_width%dsyn', exp.count_group1);
-   fileName = SingleNeuron_IF_Taivo(T_sec, 0, 'exp4',...
-       'load_state_from_file', stateFile, 'enable_learning', 0, ...
-       'experiment', exp);
-   
-   d = load(fileName);
-   
-   rates_output = [rates_output d.rate_Output];
+   if generating_states
+       % Creating state files to load from
+       fprintf('---Creating state file with %d-synapse group 1.\n', exp.count_group1);
+       outFileName = sprintf('exp4_state_width%dsyn', exp.count_group1);
+       SingleNeuron_IF_Taivo(20, 0, outFileName, 'experiment', exp);
+   else
+       % Running experiments
+       fprintf('---Running experiment with %d-synapse group 1.\n', exp.count_group1);
+       
+       stateFile = sprintf('exp4/state_files/out_exp4_state_width%dsyn', exp.count_group1);
+       fileName = SingleNeuron_IF_Taivo(T_sec, 0, 'exp4',...
+           'load_state_from_file', stateFile, 'enable_learning', 0, ...
+           'experiment', exp);
+
+       d = load(fileName);
+
+       rates_output = [rates_output d.rate_Output];
+   end;
    
 end
 %%
