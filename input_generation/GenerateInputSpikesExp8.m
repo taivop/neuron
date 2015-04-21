95b520637068e13c721669d468045457e1179e75
taivop <tpungas@ut.ee>
2015-04-22 02:13:22 +0300

    bugfix

--- input_generation/GenerateInputSpikesExp8.m
+++ input_generation/GenerateInputSpikesExp8.m
@@ -1,10 +1,10 @@
 function [spikes_binary, spiketimes] = GenerateInputSpikesExp8(exp, T0, dt, ~)
     % Get spike trains
-    [sb, st] = GenerateInputSpikesMacke(100, exp.rate, exp.input_correlation, T0, dt, '');
+    [spikes_binary, spiketimes] = GenerateInputSpikesMacke(100, exp.rate, exp.input_correlation, T0, dt, '');
 
     % Apply probability of release
     if exp.p < 1
-        [spikes_binary, spiketimes] = ProbabilityOfRelease(exp.p, sb, st);
+        [spikes_binary, spiketimes] = ProbabilityOfRelease(exp.p, spikes_binary, spiketimes);
     end;
 end
 
